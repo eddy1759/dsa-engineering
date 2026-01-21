4dba781c7a1a6fbeb4cbdcc2fcb2cbd901345515
Edet Asuquo <eddy1759@gmail.com>
2026-01-21 15:30:19 +0100

    feat(stack): implemented the largest rectangle area in histogram using monotonic stack in typescript

---
+++ patterns/stack/largest-rectangle/largest-rectangle.ts
@@ -0,0 +1,28 @@
+/**
+ * Leetcode 84: Daily Largest Rectangle in Histogram
+ * c
+ * Pattern: Stack
+ * Stack Type: Monotonic
+ * Language: Typescript
+ * Insight: Last unresolved element decides next action
+ */
+function largestRectangleArea(heights: number[]): number {
+    const stack: number[] =  [];
+    let maxArea = 0;
+    const n = heights.length;
+
+    heights.push(0)
+
+    for (let i = 0; i < n; i++) {
+        while(stack.length > 0 && heights[i] > heights[stack[stack.length - 1]]) {
+            const height = heights[stack.pop()!];
+
+            const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, height * width)
+        }
+
+        stack.push(i)
+    }
+    return maxArea
+};
+
