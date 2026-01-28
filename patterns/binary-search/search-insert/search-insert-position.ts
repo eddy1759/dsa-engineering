96070604271f2076631f3291016947d9a9f9910a
Edet Asuquo <eddy1759@gmail.com>
2026-01-28 10:11:47 +0100

    feat: binary lower bound implementation in ts and py

---
+++ patterns/binary-search/search-insert/search-insert-position.ts
@@ -0,0 +1,26 @@
+/**
+ * LeetCode 35: Search Insert Position
+ * Url: https://leetcode.com/problems/search-insert-position/description
+ * Pattern: Binary Search (Lower Bound)
+ * Purpose: Boundary correctness
+ * Time: O(log n)
+ */
+function searchInsert(nums: number[], target: number): number {
+    let left = 0;
+    let right =  nums.length - 1
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return left
+}
+
+console.log(searchInsert([1,3,5,6], 5))
